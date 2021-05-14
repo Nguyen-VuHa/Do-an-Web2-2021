0794f37fe4c5b4b5cd4ab9743ac6390e352e740f
Nguyen Vu Ha <havu20111999@gmail.com>
2021-05-14 21:47:30 +0700

    create header homepage

---
+++ public/js/home.js
@@ -0,0 +1,46 @@
+/* Header Nav Reponsive Moblie */
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', function() {
+        nav.classList.toggle('nav-active');
+        //animation
+        navLinks.forEach((link, index) => {
+            if(link.style.animation)
+            {
+                link.style.animation = ''
+            }
+            else
+            {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        
+        });
+
+        burger.classList.toggle('toggle');
+    });
+};
+
+// Menu Login Dropdown Form
+const menuToggle = () => {
+    const btnLogin = document.querySelector('.btn-dropmenu')
+    const toggleMenu = document.querySelector('.form-login');
+    btnLogin.addEventListener('click', ()=>{
+        toggleMenu.classList.toggle('active');
+    });
+}
+
+// Active click button nav
+const menuActive = () => {
+    $(document).on('click', '.nav-links li', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+   });
+}
+
+navSlide();
+menuToggle();
+menuActive();
+
+/* End Header Nav Reponsive Moblie */
