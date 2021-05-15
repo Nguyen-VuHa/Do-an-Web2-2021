043c57197ee307ae752a9225e57f915dc4ed2322
Buu Nguyen <nhbuu.17ck1@gmail.com>
2021-05-15 20:01:14 +0700

    tạo giao diện trang thông tin cá nhân

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const app = express();
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+const profRouter = require('./routers/profile');
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.render('home');
 });
+
+app.use('/prof', profRouter);
 
 const port = process.env.PORT || 3000;
 console.log(`Server is listening on port ${port}`);
