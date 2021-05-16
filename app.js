58644aae4592451ff17fff05891747218f6a6dff
Nguyen Vu Ha <havu20111999@gmail.com>
2021-05-16 18:49:09 +0700

    create page error 404

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 const profRouter = require('./routers/profile');
-
+const pg404Router = require('./routers/404');
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
@@ -13,6 +13,7 @@ app.get('/', function(req, res){
 });
 
 app.use('/prof', profRouter);
+app.use('/error', pg404Router);
 
 const port = process.env.PORT || 3000;
 console.log(`Server is listening on port ${port}`);
