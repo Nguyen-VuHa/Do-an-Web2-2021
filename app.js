6eface3857990babae0f90d59bd3ca02d4c884bf
Nguyen Vu Ha <havu20111999@gmail.com>
2021-05-16 20:42:07 +0700

    split layout with express-ejs-layout and body-parser

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
-
+// module support
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+//===========================
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.use(expressLayouts);
 
 const homeRouter = require('./routers/home');
 const profRouter = require('./routers/profile');
 const pg404Router = require('./routers/404');
-
-
-app.use(express.static('public'));
 
 app.use('/', homeRouter);
 app.use('/prof', profRouter);
