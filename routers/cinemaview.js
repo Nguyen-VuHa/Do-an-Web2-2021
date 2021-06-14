6aa6cc3c5dc8575f81e44a6018a04d79fc5377c6
Nguyen Vu Ha <havu20111999@gmail.com>
2021-06-14 10:38:12 +0700

    load data page view cinema

--- routers/cinemaview.js
+++ routers/cinemaview.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const Theaters = require('../models/theater');
 const asyncHandler = require('express-async-handler');
 const router = express.Router();
 
 router.use(function(req, res, next){
-    res.locals.title = `CGV KHTN CS 2`;
+    res.locals.title = `${req.query.name}`;
     next();
 });
 
 router.get('/',asyncHandler(async function(req, res) {
-    res.render('cinemaView');
+    if(req.query.id !== undefined)
+    {
+        const data = await Theaters.findById(req.query.id);
+        if(data)
+            res.render('cinemaView', { data });
+        else
+            res.render('page404');
+    }
+    else {
+        res.render('page404');
+    }
+
 }));
 
 module.exports = router;
