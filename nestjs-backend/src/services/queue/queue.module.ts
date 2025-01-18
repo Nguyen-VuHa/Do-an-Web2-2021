4228ff536780f654ee2042b4706160e5e4baa8d7
Nguyen-VuHa <havu20111999@gmail.com>
2025-01-18 16:57:22 +0700

    [ci] fix bull queue

--- nestjs-backend/src/services/queue/queue.module.ts
+++ nestjs-backend/src/services/queue/queue.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { QueueService } from './queue.service';
 import { BullModule } from '@nestjs/bull';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [BullModule.registerQueue({ name: 'booking' }, { name: 'email' })],
+  imports: [
+    ConfigModule, // Đảm bảo rằng ConfigModule được import
+    BullModule.registerQueueAsync(
+      {
+        name: 'booking',
+        useFactory: async (configService: ConfigService) => ({
+          redis: {
+            host: configService.get<string>('REDIS_HOST'),
+            port: configService.get<number>('REDIS_PORT'),
+            username: configService.get<string>('REDIS_USERNAME'),  // Thêm username nếu có
+            password: configService.get<string>('REDIS_PASSWORD'),  // Thêm password nếu có
+          },
+        }),
+        inject: [ConfigService],
+      },
+      {
+        name: 'email',
+        useFactory: async (configService: ConfigService) => ({
+          redis: {
+            host: configService.get<string>('REDIS_HOST'),
+            port: configService.get<number>('REDIS_PORT'),
+            username: configService.get<string>('REDIS_USERNAME'),  // Thêm username nếu có
+            password: configService.get<string>('REDIS_PASSWORD'),  // Thêm password nếu có
+          },
+        }),
+        inject: [ConfigService],
+      }
+    ),
+  ],
   providers: [QueueService],
   exports: [QueueService],
 })
